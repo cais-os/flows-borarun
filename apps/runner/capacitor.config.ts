c9f5563cd422ddcbfb839102e38bcb069a59473f
rodrigohcribeiro <rodrigohcribeiro@gmail.com>
2026-05-08 14:47:50 -0300

    fix: remove runner capacitor dev server url

--- apps/runner/capacitor.config.ts
+++ apps/runner/capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.govakreality.gudrun',
   appName: 'gudrun',
-  webDir: 'dist',
-  server: {
-    url: 'http://192.168.0.9:8080',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
